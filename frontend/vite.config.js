1c9c32cd490cae3d3dc8ce7e87733f26875d0122
Sushma Gowda <153488285+Sushmaa123@users.noreply.github.com>
2026-08-07 10:37:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['propel-ai-frontend-service.onrender.com'],
+    allowedHosts: ['propel-ai-frontend.onrender.com'],
   },
   preview: {
     host: true,
     port: 5173,
-    allowedHosts: ['propel-ai-frontend-service.onrender.com'],
+    allowedHosts: ['propel-ai-frontend.onrender.com'],
   },
 })
