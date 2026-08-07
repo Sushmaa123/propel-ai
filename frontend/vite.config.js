bbf9e220239f53c253c33f6cadd54ecba0c6000e
Sushma Gowda <153488285+Sushmaa123@users.noreply.github.com>
2026-08-07 10:40:57 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    allowedHosts: ['propel-ai-frontend.onrender.com'],
-  },
-  preview: {
-    host: true,
-    port: 5173,
-    allowedHosts: ['propel-ai-frontend.onrender.com'],
-  },
 })
